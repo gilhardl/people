eba217e11eeef53de41f00418096aaed2d50b2b5
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-07 22:23:30 +0200

    feat(api/AppModule): importTypeORM

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const typeOrmOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'people',
+  synchronize: true,
+  entities: []
+};
+
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmOptions)],
   controllers: [AppController],
   providers: [AppService]
 })
