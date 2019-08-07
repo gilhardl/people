8f56cdc289d857719e54615f5df34986d1ed6933
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-07 20:44:51 +0200

    feat(): add Material with HammerJS and custom theme

--- apps/people/src/app/app.module.ts
+++ apps/people/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
