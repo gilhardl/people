ba1b116d7ad86d2fe5865bc08bcbe781cf88dd1e
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-07 20:31:42 +0200

    feat(): add routing

--- apps/people/src/app/app.module.ts
+++ apps/people/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
