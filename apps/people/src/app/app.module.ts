c346d8ab39b20c2c5f9595ed7d7f66353f115d50
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-07 20:25:49 +0200

    fix(): remove useless import

--- apps/people/src/app/app.module.ts
+++ apps/people/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
