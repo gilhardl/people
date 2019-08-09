93aab47c9309538424e9f3042d2b41639c2df7de
Lucas GILHARD <l.gilhard@cyberg.fr>
2019-08-09 17:11:36 +0200

    feat(api/UsersModule): implements Users controller that implements CrudController

--- apps/api/src/app/users/users.controller.ts
+++ apps/api/src/app/users/users.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
 
+import { Crud, CrudController } from '@nestjsx/crud';
+
+import { User } from '../entities/user.entity';
+import { UsersService } from './users.service';
+
+@Crud({
+  model: {
+    type: User,
+  },
+})
 @Controller('users')
-export class UsersController {}
+export class UsersController implements CrudController<User> {
+  constructor(public service: UsersService) { }
+}
