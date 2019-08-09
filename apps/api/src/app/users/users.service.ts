4774a1807903b9a56ef0e1d620ec908100fffddb
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-09 16:23:39 +0200

    feat(api:UsersService): implement service to extend TypeOrmCrudService

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+
+import { User } from '../entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService extends TypeOrmCrudService<User> {
+  constructor(@InjectRepository(User) repo) {
+    super(repo);
+  }
+}
