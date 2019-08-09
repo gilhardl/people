676d2671475b84be1edc0c6ac3334ba6ef8ecf87
Lucas GILHARD <l.gilhard@cyberg.fr>
2019-08-09 16:20:56 +0200

    feat(api/UsersModule): declare User entity as TypeORM repository feature

--- apps/api/src/app/users/users.module.ts
+++ apps/api/src/app/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+import { User } from '../entities/user.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
