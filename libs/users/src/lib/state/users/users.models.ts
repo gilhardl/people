a8602c621ce9b64ff1a760996b12fec2568f5aa5
Lucas GILHARD <l.gilhard@cyberg.fr>
2019-08-09 14:51:22 +0200

    feat(users): update user model

--- libs/users/src/lib/state/users/users.models.ts
+++ libs/users/src/lib/state/users/users.models.ts
@@ -2,5 +2,15 @@
  * Interface for the 'Users' data
  */
 export interface UsersEntity {
-  id: string | number; // Primary ID
+  id: number;
+  username: string;
+  password: string;
+  confirmationToken: string;
+  recoverToken: string;
+  role: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  job: string;
 }
