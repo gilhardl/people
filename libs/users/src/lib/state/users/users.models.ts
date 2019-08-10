0852b5244c7435729684080855fabaa0b0394004
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-10 16:32:22 +0200

    feat(users/UsersEntity): define some properties as optionnals

--- libs/users/src/lib/state/users/users.models.ts
+++ libs/users/src/lib/state/users/users.models.ts
@@ -4,9 +4,9 @@
 export interface UsersEntity {
   id: number;
   username: string;
-  password: string;
-  confirmationToken: string;
-  recoverToken: string;
+  password?: string;
+  confirmationToken?: string;
+  recoverToken?: string;
   role: string;
   firstname: string;
   lastname: string;
