e81303b5df3665ec6c021b618f159d9cdb3ba9ac
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-10 13:12:56 +0200

    feat(api/UsersService): implement findOneByUsername

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
+import { Repository, Equal } from 'typeorm';
+
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
 import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService extends TypeOrmCrudService<User> {
-  constructor(@InjectRepository(User) repo) {
+  constructor(@InjectRepository(User) repo: Repository<User>) {
     super(repo);
+  }
+
+  async findOneByUsername(username: string) {
+    const users: User[] = await this.repo.find({
+      username: Equal(username)
+    });
+    return users.length > 0 ? users[0] : null;
   }
 }
