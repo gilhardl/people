db965b5ec58d4db3325783fe22bf96738eac9822
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-10 16:15:24 +0200

    feat(api/AuthModule): add Passport local strategy

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+import { LocalStrategy } from './local.strategy';
+
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
