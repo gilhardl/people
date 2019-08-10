2fb053040b50dd967313ef80820511946b1756e8
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-10 17:29:16 +0200

    feat(api/AuthModule): add a signin method to AuthService, called from the sign in route
    this method return a jwt token in addition of the user

--- apps/api/src/app/auth/auth.module.ts
+++ apps/api/src/app/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { environment } from '../../environments/environment';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -9,7 +12,14 @@ import { LocalStrategy } from './local.strategy';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: environment.jwtSecret,
+      signOptions: { expiresIn: '60s' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
 import { UsersEntity } from '@people/users';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
   async validateUser(username: string, pass: string): Promise<UsersEntity> {
     const user = await this.usersService.findOneByUsername(username);
@@ -15,5 +18,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async signin(user: UsersEntity) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      jwt: this.jwtService.sign(payload),
+      user: user
+    };
   }
 }
