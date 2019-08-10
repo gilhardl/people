8f7ef0acc727e5f6e3b30dbe7ba8fc4e703334f7
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-10 16:11:56 +0200

    feat(api/AuthService): update return type

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 import { UsersService } from '../users/users.service';
 
+import { UsersEntity } from '@people/users';
+
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<UsersEntity> {
     const user = await this.usersService.findOneByUsername(username);
     if (user && user.password === pass) {
       const { password, ...result } = user;
