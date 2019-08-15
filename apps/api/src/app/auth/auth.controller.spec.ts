79ff2f9763f903d8a2094e2f68df762d2cd47104
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-15 23:11:13 +0200

    test(AuthController): add unit tests

--- apps/api/src/app/auth/auth.controller.spec.ts
+++ apps/api/src/app/auth/auth.controller.spec.ts
@@ -11,6 +11,7 @@ import { UsersService } from '../users/users.service';
 import { User } from '../entities/user.entity';
 
 import * as mocks from '../users/__mocks__/users';
+import { HttpException } from '@nestjs/common';
 
 const MockRepository = jest.fn().mockImplementation(() => {
   return {
@@ -24,6 +25,7 @@ const MockRepository = jest.fn().mockImplementation(() => {
 describe('Auth Controller', () => {
   let controller: AuthController;
   let service: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -42,6 +44,7 @@ describe('Auth Controller', () => {
 
     controller = module.get<AuthController>(AuthController);
     service = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
@@ -62,6 +65,94 @@ describe('Auth Controller', () => {
       const result = await controller.signin({ user: user });
 
       expect(result).toMatchObject(expected);
+    });
+  });
+
+  describe('register', () => {
+    it("should throw an exception if user's email address isn't available", async () => {
+      const { password, confirmationToken, recoverToken, ...user } = mocks.user;
+      user.email = user.email.split('@')[0] + '@fakedomain.com'; // Ensure to don't throw 'banned-email-domain' exception
+      user.username = user.email; // The frontend use user email as username
+
+      expect(
+        controller.register({
+          username: 'john.doe@gmail.com',
+          email: 'john.doe@gmail.com',
+          firstname: 'john',
+          lastname: 'doe',
+          confirmationUrl: 'http://localhost:4200/#/auth/confirm'
+        })
+      ).rejects.toThrow('banned-email-domain');
+
+      jest
+        .spyOn(usersService, 'isEmailAvailable')
+        .mockImplementation(async () => false);
+
+      expect(
+        controller.register({
+          username: user.username,
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          confirmationUrl: 'http://localhost:4200/#/auth/confirm'
+        })
+      ).rejects.toThrow('email-not-available');
+    });
+
+    // it('should send an email to new user with a link for user confirmation, based on given confirmationUrl and containing confirmation token', () => {});
+
+    it('should return true (success)', async () => {
+      const { password, confirmationToken, recoverToken, ...user } = mocks.user;
+      user.email = user.email.split('@')[0] + '@fakedomain.com'; // Ensure to don't throw 'banned-email-domain' exception
+      user.username = user.email; // The frontend use user email as username
+
+      const expected = {
+        user: user,
+        token: 'super-secret-uuid-token'
+      };
+
+      jest
+        .spyOn(usersService, 'isEmailAvailable')
+        .mockImplementation(async () => true);
+
+      jest.spyOn(service, 'register').mockImplementation(async () => expected);
+
+      const result = await controller.register({
+        username: user.username,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        confirmationUrl: 'http://localhost:4200/#/auth/confirm'
+      });
+
+      expect(result).toBe(true);
+    });
+
+    it('should throw an exception (error)', () => {
+      const { password, confirmationToken, recoverToken, ...user } = mocks.user;
+      user.email = user.email.split('@')[0] + '@fakedomain.com'; // Ensure to don't throw 'banned-email-domain' exception
+      user.username = user.email; // The frontend use user email as username
+
+      const expected = {
+        user: null,
+        token: ''
+      };
+
+      jest
+        .spyOn(usersService, 'isEmailAvailable')
+        .mockImplementation(async () => true);
+
+      jest.spyOn(service, 'register').mockImplementation(async () => expected);
+
+      expect(
+        controller.register({
+          username: user.username,
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          confirmationUrl: 'http://localhost:4200/#/auth/confirm'
+        })
+      ).rejects.toThrow('user-creation-error');
     });
   });
 });
