a537961955ae0aa9104925ad9461377c569efa47
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-16 00:29:54 +0200

    feat(api): set up Sendgrid

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { environment } from "../environments/environment";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { User } from "./entities/user.entity";
-import { UsersModule } from "./users/users.module";
-import { AuthModule } from "./auth/auth.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import * as sgMail from '@sendgrid/mail';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { User } from './entities/user.entity';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const typeOrmOptions: TypeOrmModuleOptions = {
-  type: "mysql",
+  type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
