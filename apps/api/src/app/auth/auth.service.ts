f700a78d3d7401119861155d36739b4bc581e61b
Lucas GILHARD <l.gilhard@gmail.com>
2019-08-16 17:30:19 +0200

    feat(api/AuthService): implement initiateConfirmation and confirm
    also update validateUser

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+import * as moment from 'moment';
+
 import * as uuid from 'uuid/v1';
+import * as uuidTime from 'uuid-time';
+import * as crypto from 'crypto';
 
 import { UsersEntity } from '@people/users';
 
@@ -17,7 +21,12 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<UsersEntity> {
     const user = await this.usersService.findOneByUsername(username);
-    if (user && user.password === pass) {
+    const hashPass = crypto
+      .createHash('md5')
+      .update(pass)
+      .digest('hex');
+
+    if (user && user.confirmed && user.password === hashPass) {
       const { password, confirmationToken, recoverToken, ...result } = user;
       return result;
     }
@@ -51,5 +60,34 @@ export class AuthService {
       user: result,
       token: confirmationToken
     };
+  }
+
+  async initiateConfirmation(token: string) {
+    const tokenDate = moment(uuidTime.v1(token));
+    const limitDate = moment().subtract(1, 'hour');
+    if (tokenDate.isBefore(limitDate)) {
+      return false;
+    }
+
+    const user = await this.usersService.findOneByConfirmationToken(token);
+    return user ? true : false;
+  }
+
+  async confirm(token: string, pass: string) {
+    let user: User = await this.usersService.findOneByConfirmationToken(token);
+    if (!user) {
+      return null;
+    }
+
+    user.password = crypto
+      .createHash('md5')
+      .update(pass)
+      .digest('hex');
+    user.confirmationToken = '';
+    user.confirmed = true;
+
+    user = await this.usersService.confirm(user);
+    const { password, confirmationToken, recoverToken, ...result } = user;
+    return result;
   }
 }
